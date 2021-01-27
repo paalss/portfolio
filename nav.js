6cf3b7e7dd71c521184a9c65401ef9795aed7c84
paalss <paalss94@gmail.com>
2021-01-27 14:35:56 +0100

    Omstrukturering, flytting av kode, småfiks

--- nav.js
+++ nav.js
@@ -5,12 +5,14 @@ nav.innerHTML = `
   <div class="container">
     <ul>
       <li><a href="../index.html">Hjem</a></li>
-      <li><a href="rankingapp.html">Ranking app</a></li>
-      <li><a href="videoopplasting.html">Videoopplastingssystemer</a></li>
-      <li><a href="maclean.html">Alistair Maclean</a></li>
+      <div class="work-links">
+        <li><a href="rankingapp.html">Ranking app</a></li>
+        <li><a href="videoopplasting.html">Videoopplastingssystemer</a></li>
+        <li><a href="maclean.html">Alistair Maclean</a></li>
+      </div>
     </ul>
   </div>
 `
 
-const openLiA = nav.querySelector(`ul li:nth-child(${pageListPlace})`).nextElementSibling.querySelector('a')
+const openLiA = nav.querySelector(`div.work-links li:nth-child(${pageListPlace})`).querySelector('a')
 openLiA.classList.add('open')

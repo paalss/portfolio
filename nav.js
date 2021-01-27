bfdbdfd03703f0bc534abdf1c8709e57f9a01954
paalss <paalss94@gmail.com>
2021-01-27 16:06:25 +0100

    Putter nettsidelenkene i navbar'en inn i en sub-menu / dropdown
    For å få bedre plass på mindre skjermer og evt. hvis jeg putter inn flere nettsidelenker

--- nav.js
+++ nav.js
@@ -3,16 +3,19 @@ const pageListPlace = document.currentScript.getAttribute('pageListPlace')
 
 nav.innerHTML = `
   <div class="container">
-    <ul>
+    <ul class="nav">
       <li><a href="../index.html">Hjem</a></li>
-      <div class="work-links">
-        <li><a href="rankingapp.html">Ranking app</a></li>
-        <li><a href="videoopplasting.html">Videoopplastingssystemer</a></li>
-        <li><a href="maclean.html">Alistair Maclean</a></li>
-      </div>
+      <li><a href="#" class="open">Nettsider</a>
+        <ul class="website-links">
+          <li><a href="rankingapp.html">Ranking app</a></li>
+          <li><a href="videoopplasting.html">Videoopplastingssystemer</a></li>
+          <li><a href="maclean.html">Alistair Maclean nettside redesign</a></li>
+        </ul>
+      </li>
     </ul>
   </div>
 `
 
-const openLiA = nav.querySelector(`div.work-links li:nth-child(${pageListPlace})`).querySelector('a')
-openLiA.classList.add('open')
+// openLiAnchor er den <a> lenken som samsvarer med den siden du har åpen. Gi denne <a> en class slik at CSS kan utheve den
+const openLiAnchor = nav.querySelector(`ul.website-links li:nth-child(${pageListPlace})`).querySelector('a')
+openLiAnchor.classList.add('open')

2c333a531567bfcc0d19997510d519d0bee37191
paalss <paalss94@gmail.com>
2021-10-20 12:07:30 +0200

    SquareLink styles til module

--- src/components/common/SquareLink/SquareLink.js
+++ src/components/common/SquareLink/SquareLink.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import classes from "./SquareLink.module.css"
 
 
 function SquareLink({ link, imgSrc, imgAlt, title, tools, thereAreMoreTools = false }) {
   return (
-    <Link to={link} className="box">
-      <div className="frame">
+    <Link to={link} className={classes.square}>
+      <div className={classes.frame}>
         <img src={imgSrc} alt={imgAlt} />
       </div>
       <h3>{title}</h3>

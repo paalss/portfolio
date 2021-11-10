15634209413a97c4ba648c449be2ccbe02131c0d
paalss <paalss94@gmail.com>
2021-11-10 21:56:38 +0100

    oppdaterte dokker beskrivelse

--- src/pages/Dokker.js
+++ src/pages/Dokker.js
@@ -1,4 +1,8 @@
 import dokkerInterface from "../images/dokker/dashboard/latest-document-assignment.png";
+// import newcardImg from "../images/dokker/newscard/wip3-2021-06-30-at 08.58.07.png";
+// import noElementsFoundImg from "../images/dokker/no-elements-found/empty-state-for-your-assignments.png";
+
+// import Figure from "../components/common/Figure";
 
 function Dokker() {
   return (
@@ -30,32 +34,36 @@ function Dokker() {
         web-applikasjonen Dokker og tok et React kurs. Jeg fikk bidratt med nye
         komponenter, refaktorering, vurdering av andres kode og litt bug-fixing.
       </p>
-      {/* <p>
-        De fire første månedene av praksisen tok jeg et nettkurs i React for å
-        få den nødvendige kompetansen jeg trengte. Mot slutten av juni begynte
-        jeg å bidra på Dokker-koden.
-      </p> */}
+
+      {/* <h3>Bidro med bl.a.:</h3>
+      <Figure imgSrc={newcardImg} imgAlt="NewsCard" caption="Lagde et newscard som vises på dashboard" />
+      <Figure
+        imgSrc={noElementsFoundImg}
+        imgAlt="NewsCard"
+        caption="Refaktorerte dette til et gjenbrukbart komponent, slik at det kunne bli brukt andre steder"
+      /> */}
+
       <h3>Litt om Dokker</h3>
-      <p>
-        Fra <a href="https://www.dokker.com/nb/blog">bloggen deres</a> :
-      </p>
       <blockquote cite="https://www.dokker.com/nb/blog/villiam-ovesen-ny-salgssjef-i-learning-moment">
         Dokker er et samhandlingsverktøy med over 10 000 brukere innenfor skole,
-        tiltaksarbeid, integrering og bedrifter generelt. <br /> [...] <br />{" "}
-        Med Dokker tar man brukermedvirkning til et nytt nivå, og gir alle
-        parter i tiltaksarbeid en plattform for samhandling, utvikling og
-        dokumentasjon.
+        tiltaksarbeid, integrering og bedrifter generelt.
       </blockquote>
-      {/* <p>
-        Den har mye av funksjonaliteten som man finner i systemer som
-        ItsLearning og vitnemålsportalen: lage oppgaver og levere inn
-        besvarelser og laste opp sertifiseringer. I tillegg beholder brukerne
-        tilgangen på dokumentasjonen på egen kunnskap livet ut.
-      </p> */}
-      <b>Mer info:</b>
+      <p>
+        Fra <a href="https://www.dokker.com/nb/blog">Dokker bloggen</a>.
+      </p>
+      <p>
+        I Dokker kan f.eks. lærere legge ut oppgaver, og elever kan levere inn
+        besvarelser sammen med selvvurderinger. Elevene får samlet opp
+        dokumentasjon av egen kunnskap på Dokker, og den får de beholde
+        tilgangen til livet ut.
+      </p>
+      <b>Videre lesing:</b>
       <ul>
         <li>
           <a href="https://www.dokker.com/nb">dokker.com</a>
+        </li>
+        <li>
+          <a href="https://www.dokker.com/nb/blog">Dokker blogg</a>
         </li>
       </ul>
     </>

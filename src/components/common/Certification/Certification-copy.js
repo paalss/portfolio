3c454316d5744fc649132c392fc4572f72f919ab
paalss <paalss94@gmail.com>
2021-11-13 21:42:39 +0100

    dette er kanskje nærmere den opprinnelige koden, men det gjør fortsatt det samme som koden i forrige commit

--- src/components/common/Certification/Certification-copy.js
+++ src/components/common/Certification/Certification-copy.js
@@ -3,40 +3,39 @@ import reactCertificate from "../../../images/certifications/react-certificate.j
 import { useCallback, useEffect, useState } from "react";
 
 function Certification({ companyLogo, title, timepsan, company }) {
-  function map(val, minA, maxA, minB, maxB) {
-    return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
-  }
-
-  const Card3D = useCallback((card, ev) => {
-    let img = card.querySelector("img");
-    let imgRect = card.getBoundingClientRect();
-    let width = imgRect.width;
-    let height = imgRect.height;
-    let mouseX = ev.offsetX;
-    let mouseY = ev.offsetY;
-    let rotateY = map(mouseX, 0, 180, -25, 25);
-    let rotateX = map(mouseY, 0, 250, 25, -25);
-    let brightness = map(mouseY, 0, 250, 1.5, 0.5);
-
-    img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-    img.style.filter = `brightness(${brightness})`;
-  }, [])
-
   useEffect(() => {
-    const cards = document.querySelectorAll(".card3d")
+    function map(val, minA, maxA, minB, maxB) {
+      return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
+    }
+
+    function Card3D(card, ev) {
+      let img = card.querySelector("img");
+      let imgRect = card.getBoundingClientRect();
+      let mouseX = ev.offsetX;
+      let mouseY = ev.offsetY;
+      let rotateY = map(mouseX, 0, 180, -25, 25);
+      let rotateX = map(mouseY, 0, 250, 25, -25);
+      let brightness = map(mouseY, 0, 250, 1.5, 0.5);
+
+      img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+      img.style.filter = `brightness(${brightness})`;
+    }
+
+    var cards = document.querySelectorAll(".card3d");
+
     cards.forEach((card) => {
       card.addEventListener("mousemove", (ev) => {
         Card3D(card, ev);
       });
-  
+
       card.addEventListener("mouseleave", (ev) => {
         let img = card.querySelector("img");
-  
+
         img.style.transform = "rotateX(0deg) rotateY(0deg)";
         img.style.filter = "brightness(1)";
       });
     });
-  }, [Card3D]);
+  });
 
   // <div className="card3d" style={{ width: "300px" }}></div>
   // <div className="card3d" style={{ width: "300px" }}>

d924a5a40793524ac0f1b58d99d7c385283a098c
paalss <paalss94@gmail.com>
2022-01-16 13:58:31 +0100

    oppdaterte EO tekst

--- src/pages/Extraoptical.js
+++ src/pages/Extraoptical.js
@@ -28,26 +28,19 @@ function Extraoptical() {
         <dt>PHTML</dt>
         <dd>
           Et HTML templating språk (HTML som aksepterer variabler og
-          expressions) i likhet med Twig og Handlebars.
+          expressions). Det er ikke så ulikt Twig og Handlebars.
         </dd>
       </dl>
       <p>
-        I dennne praksisen jobbet jeg med nettbutikken Extra Optical. Det har
-        for det meste gått i den norske versjonen, men har også gjort noe på de
-        andre språkene.
+        I dennne praksisen jobbet jeg med nettbutikken Extra Optical (frontend).
+        EO skulle gå over til å bygge nettsiden sin på nytt i en <i>page builder</i> da jeg hadde vært
+        der i ca. 2 måneder, så jeg har fått jobbet på både ny og gammel versjon.
       </p>
-      {/* <p>
-        Arbeidsflyten har stort sett vært at jeg logger inn i <i>magento admin</i> &shy;
-        web-appen, åpner en <i>page</i>, <i>block</i> eller meny, og fikser på
-        HTML-koden som er der. Oftest oppretter jeg nye filer lokalt i VSCode og
-        limer inn HTML-koden som var der fra før inn i den, for backup. Jeg gjør
-        også endringene mine i VSCode, siden Magento ikke har noe{" "}
-        <i>syntax-highlighting</i> eller innrykk i koden.
-      </p> */}
-      <p>Litt oppsummering av det jeg har gjort til nå:</p>
+      <h3>Bidrag</h3>
       <ul className="normal-list">
         <li>
-          Fikset errors og kodeslurv i HTML
+          Fikset errors og kodeslurv i HTML. For eksempel var det mange enslige{" "}
+          <i>opening/closing</i>-HTML-tags som manglet <i>matching pair</i>.
           {/* <ul>
             <li>
               <T>/a</T>-tags som manglet starting tag. Og{" "}
@@ -56,7 +49,7 @@ function Extraoptical() {
             </li>
             <li>
               <T>h1</T> og <T>div</T> som var{" "}
-              <em>inni</em> en <T>p</T>. Dette gjør at nettlesren
+              <em>inni</em> en <T>p</T>. Dette gjør at nettleseren
               automatisk rendrer en <T>/p</T> rett før{" "}
               <T>h1</T>-tagen starter, og den <T>/p</T>
               -taggen som man selv har plassert i koden, blir én for mye.
@@ -66,37 +59,38 @@ function Extraoptical() {
               om side
             </li>
             <li>
-              Kunne også se koden: <code><T>p</T><T>h3</T><T>strong</T><T>/strong</T>
-              <T>/h3</T><T>/p</T></code></li> et sted
+              Kunne også se koden: <code><T>p</T><T>h3</T><T>strong</T><T>/strong</T><T>/h3</T><T>/p</T></code></li> et sted
+
+              <p><h3><strong></strong></h3></p>
+              <p><img style="float: left; margin-right: 50px;" src="{{media url=wysiwyg/knowledgecenter/round-pic.jpg}}" width="200" /><strong></strong></p>
           </ul> */}
         </li>
         <li>
           Rettet opp / fjernet <i>broken links</i>
         </li>
-        {/* <li>Lagt til nye lenketekster</li> */}
+        <li>Lagd undersider {/*ut fra tilsendte bilder og tekst*/}</li>
+        <li>Fikset på Extra Opticals Zendesk Help center.</li>
         <li>
-          Fikset{" "}
-          <a
-            href="https://extraoptical.no/ray-ban"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ray-ban-siden
-          </a>{" "}
-          for mobilvisning
-        </li>
-        <li>Lagd undersider ut fra tilsendte bilder og tekst</li>
-        <li>Fikset på Extra Opticals Zendesk Help center</li>
-        <li>
-          Fikset layout
+          Fikset layout på bunntekst og ordnet en side for mobilstørrelser
           {/* <ul>
             <li>Kopiert styling over flere undersider</li>
             <li>
               fjernet unødvendig bruk av flere <T>h1</T> pr sidedokumentet
             </li>
+            <li>
+              Fikset{" "}
+              <a
+                href="https://extraoptical.no/ray-ban"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ray-ban-siden
+              </a>{" "}
+              for mobilvisning
+            </li>
           </ul> */}
         </li>
-        <li>Fikset litt typos</li>
+
         {/* <li>
           Rettet opp i ting som ikke gir helt mening{" "}
           <ul>
@@ -119,11 +113,17 @@ function Extraoptical() {
         <li>
           CSS-en er helt <i>all over the place</i>. Mye <i>inline styling</i>,
           noe i egne <T>style</T>-tags sidedokumentet og i{" "}
-          <T>Head</T>. Og så er det brukt et eksternt stilark.{" "}
-          <br /> Jeg må tilstå at jeg selv har bidratt litt å skrive CSS flere
-          steder - skulle helst ha hatt tilgang til det eksterne stilarket, men
+          <T>Head</T>. Og så er det brukt et eksternt stilark, som kun Tinyelephants
+          utviklerne har tilgang til. Det er kanskje verdt å nevne at jeg aldri har hatt
+          tilgang til noe kildekode til Extra Optical nettsiden. Det eneste jeg har er
+          tilgang til et CMS som har valgt mer usability fremfor hackability.
+          Det mest hackability-e man kan gjøre er å legge inn egne elementer - kalt blocks
+          - hvor man kan legge inn HTML kode (og i pwa-page-builder-siden så kan man legge
+            inn page elements).{" "}
+          <br /> Jeg skulle gjerne ha hatt tilgang til CSS-stilark(et/ene), men
           har blitt bedt om å sende koden til ukrainerne slik at de kan gjøre
-          endringen på sin maskin.
+          endringen på sin maskin. De tar også timebetalt av å snakke/chatte med
+          oss.
         </li>
         <li>Koden har sjelden noe form for innrykk</li>
         <li>

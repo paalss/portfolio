306ffffbf0c94ddb252bfb8a3490e44873a72b9e
paalss <paalss94@gmail.com>
2022-09-08 12:19:09 +0200

    barchart experience update
    - increase React experience
    - decrease mySQL & relational databases

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,6 +23,7 @@ const Home = () => (
         web={[
           { tech: "HTML & CSS", level: "3" },
           { tech: "JavaScript", level: "3" },
+          { tech: "React", level: "3" },
           {
             // denne string'en passer for bar title, men ikke som label innerhtml...
             tech: t`MySQL & relasjonsdatabaser`,
@@ -30,9 +31,8 @@ const Home = () => (
             techHtmlEntities: (
               <Trans>MySQL &amp; relasjons&shy;databaser</Trans>
             ),
-            level: "3",
+            level: "2",
           },
-          { tech: "React", level: "2" },
           { tech: "PHP", level: "2" },
           { tech: t`Automatisk testing`, level: "1" },
           { tech: "NodeJS", level: "1" },

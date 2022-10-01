8b2c096b871b7c00e6cf612690f6e4783ef62b48
paalss <paalss94@gmail.com>
2022-10-01 14:25:11 +0200

    add darkmode

--- src/components/layout/Nav/Nav.js
+++ src/components/layout/Nav/Nav.js
@@ -1,51 +1,60 @@
-import React from "react";
+import React, { Suspense } from "react";
 import NavLink from "next/link";
 import pagesInfo from "../../../pagesInfo";
 import LangMenu from "../../common/LangMenu";
+
 // import DarkmodeSwitch from "../../common/DarkmodeSwitch";
 
 import classes from "./Nav.module.css";
+import dynamic from "next/dynamic";
 
-const Nav = () => (
-  <nav className={classes.nav}>
-    <div className="container">
-      <ul className={classes.ul}>
-        {/* linker til subpages */}
-        {pagesInfo.map((page) => (
-          <li key={page.id}>
-            <NavLink
-              href={page.link}
-              activeClassName={classes.selected}
-              exact={page.exact ? page.exactActiveClassName : false}
-            >
-              <a>{page.title}</a>
-            </NavLink>
-            {page.subpages && (
-              <ul className="website-links">
-                {page.subpages.map((subpage) => (
-                  <li key={subpage.id}>
-                    <NavLink
-                      href={subpage.link}
-                      activeClassName={classes.selected}
-                    >
-                      <a>{subpage.title}</a>
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            )}
+const Nav = () => {
+  let DarkmodeSwitch = undefined
+  if (typeof window !== "undefined") {
+    DarkmodeSwitch = dynamic(() => import("../../common/DarkmodeSwitch"));
+  }
+
+  return (
+    <nav className={classes.nav}>
+      <div className="container">
+        <ul className={classes.ul}>
+          {/* linker til subpages */}
+          {pagesInfo.map((page) => (
+            <li key={page.id}>
+              <NavLink
+                href={page.link}
+                activeClassName={classes.selected}
+                exact={page.exact ? page.exactActiveClassName : false}
+              >
+                <a>{page.title}</a>
+              </NavLink>
+              {page.subpages && (
+                <ul className="website-links">
+                  {page.subpages.map((subpage) => (
+                    <li key={subpage.id}>
+                      <NavLink
+                        href={subpage.link}
+                        activeClassName={classes.selected}
+                      >
+                        <a>{subpage.title}</a>
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+          {/* knapper for innstillinger */}
+          <li>
+            <LangMenu />
           </li>
-        ))}
-        {/* knapper for innstillinger */}
-        <li>
-          <LangMenu />
-        </li>
-        <li className={classes.switch}>
-          {/* <DarkmodeSwitch /> */}
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+          <li className={classes.switch}>
+            {DarkmodeSwitch && <DarkmodeSwitch />}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;

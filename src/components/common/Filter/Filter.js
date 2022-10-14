687040d93ce741333dfc52f43b486c554473b834
paalss <paalss94@gmail.com>
2022-10-14 22:39:09 +0200

    optimize Filter images
    replace img with Image from next/image

--- src/components/common/Filter/Filter.js
+++ src/components/common/Filter/Filter.js
@@ -1,30 +1,52 @@
 import React from "react";
+import Image from "next/image";
 
 import classes from "./Filter.module.css";
 
+import typescriptLogo from "../../../../public/images/tools/typescript-icon.svg";
+import javascriptLogo from "../../../../public/images/tools/javascript.svg";
+import nextLogo from "../../../../public/images/tools/nextjs-icon.svg";
+import phpLogo from "../../../../public/images/tools/php.svg";
+import reactLogo from "../../../../public/images/tools/react.svg";
+import graphqlLogo from "../../../../public/images/tools/graphql.svg";
+import cssLogo from "../../../../public/images/tools/css-3.svg";
+import htmlLogo from "../../../../public/images/tools/html-5.svg";
+import litLogo from "../../../../public/images/tools/lit-icon.svg";
+import mysqlLogo from "../../../../public/images/tools/mysql-icon.svg";
+import dockerLogo from "../../../../public/images/tools/docker-icon.svg";
+import jestLogo from "../../../../public/images/tools/jest.svg";
+
 const findMatchingImage = (name) => {
   const images = [
-    "/images/tools/javascript.svg",
-    "/images/tools/typescript-icon.svg",
-    "/images/tools/nextjs-icon.svg",
-    "/images/tools/php.svg",
-    "/images/tools/react.svg",
-    "/images/tools/graphql.svg",
-    "/images/tools/css-3.svg",
-    "/images/tools/html-5.svg",
-    "/images/tools/lit-icon.svg",
-    "/images/tools/mysql-icon.svg",
-    "/images/tools/docker-icon.svg",
-    "/images/tools/jest.svg",
+    javascriptLogo,
+    typescriptLogo,
+    nextLogo,
+    phpLogo,
+    reactLogo,
+    graphqlLogo,
+    cssLogo,
+    htmlLogo,
+    litLogo,
+    mysqlLogo,
+    dockerLogo,
+    jestLogo,
   ];
 
   const lowerCaseName = name.toLowerCase();
-  const image = images.find((e) => e.includes(lowerCaseName));
+  const image = images.find((e) => {
+    // e.src === /_next/static/media/javascript.9b50b767.svg
+    const filename = e.src.split("/")[4];
+    // filename === javascript.9b50b767.svg
+    return filename.includes(lowerCaseName);
+  });
   return image;
 };
 
 const Checkbox = ({ name: techName, isChecked, onChange, amount }) => {
-  const src = findMatchingImage(techName);
+  const { src, width: intrinsicWidth, height: intrinsicHeight } = findMatchingImage(techName);
+  const renderedHeight = 30;
+  const proportion = intrinsicHeight / renderedHeight;
+  const renderedWidth = intrinsicWidth / proportion;
   const optionClass = isChecked
     ? classes.option + " " + classes.checked
     : classes.option;
@@ -35,7 +57,12 @@ const Checkbox = ({ name: techName, isChecked, onChange, amount }) => {
         className={optionClass}
         onClick={() => onChange(techName)}
       >
-        <img src={src} alt={techName} height={30} />
+        <Image
+          src={src}
+          alt={techName}
+          height={renderedHeight}
+          width={renderedWidth}
+        />
         {techName} ({amount})
       </button>
     </div>

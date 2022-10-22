2646fceaf1492f831fe0bd17b6d293c259c457ee
paalss <paalss94@gmail.com>
2022-10-22 18:02:00 +0200

    add blur placeholder to work header images

--- src/components/common/WorkHeaderImage/WorkHeaderImage.js
+++ src/components/common/WorkHeaderImage/WorkHeaderImage.js
@@ -1,10 +1,15 @@
-import Image from "next/future/image";
+import FutureImage from "next/future/image";
 import React from "react";
 import classes from "./WorkHeaderImage.module.css";
 
 const WorkHeaderImage = (props) => {
   return (
-      <Image {...props} className={classes.workHeaderImage} />
+    <FutureImage
+      {...props}
+      placeholder="blur"
+      blurDataURL={props.src}
+      className={classes.workHeaderImage}
+    />
   );
 };
 
